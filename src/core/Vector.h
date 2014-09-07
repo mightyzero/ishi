faf77ad032bbd22907c6452cb5f609253a06ebb7
Minh Pham <minh.pham@utexas.edu>
2014-09-07 10:29:04 -0500

    Forgot to add Vector comment-out from a previous commit.

--- src/core/Vector.h
+++ src/core/Vector.h
@@ -2,72 +2,78 @@
 #define NF_VECTOR_H
 
 #include <core/Common.h>
-#include <blitz/array.h>
 
 /*=============================================================================+
  | Class Forward Declarations
  +============================================================================*/
-template <typename T, uint32_t D>
+template<size_t D, typename T>
 class Vector;
 
 /*=============================================================================+
  | Function Prototypes                                                         |
  +============================================================================*/
 /// \brief Add two vectors and return the result.
-template <typename T, uint32_t D>
-Vector<T,D> operator+(const Vector<T,D>& v1, const Vector<T,D>& v2);
+template<size_t D, typename T>
+Vector<D,T> operator+(const Vector<D,T>& v1, const Vector<D,T>& v2);
 
 /// \brief Scale a vector by a scalar and return the result.
 template <typename T, uint32_t D, typename T2,
           Where<std::is_integral<T2>>>
           // typename std::enable_if<std::is_integral<T2>::value,int>::type = 0>
-Vector<T,D> operator*(const Vector<T,D>& vec, const T2& factor);
+Vector<D,T> operator*(const Vector<D,T>& vec, const T2& factor);
 
 /// \brief Scale a vector by a scalar and return the result.
 template <typename T, uint32_t D, typename T2,
           typename std::enable_if<std::is_integral<T2>::value,int>::type = 0>
-Vector<T,D> operator*(const T2& factor, const Vector<T,D>& vec);
+Vector<D,T> operator*(const T2& factor, const Vector<D,T>& vec);
 
 /// \brief Divide a vector by a scalar and return the result.
 template <typename T, uint32_t D, typename T2,
           typename std::enable_if<std::is_integral<T2>::value,int>::type = 0>
-Vector<T,D> operator/(const Vector<T,D>& vec, const T2& factor);
+Vector<D,T> operator/(const Vector<D,T>& vec, const T2& factor);
 
 /// \brief Negate a vector and return the result.
-template <typename T, uint32_t D>
-Vector<T,D> operator-(const Vector<T,D>& vec);
+template<size_t D, typename T>
+Vector<D,T> operator-(const Vector<D,T>& vec);
 
 /// \brief Compute the dot product of two vectors.
-template <typename T, uint32_t D>
-T dot(const Vector<T,D>& vec1, const Vector<T,D>& vec2);
+template<size_t D, typename T>
+T dot(const Vector<D,T>& vec1, const Vector<D,T>& vec2);
 
 /// \brief Compute the cross product of two vectors.
-template <typename T, uint32_t D>
-Vector<T,D> cross(const Vector<T,D>& vec1, const Vector<T,D>& vec2);
+template<size_t D, typename T>
+Vector<D,T> cross(const Vector<D,T>& vec1, const Vector<D,T>& vec2);
 
 /// \brief Compute the square of the vector's length.
-template <typename T, uint32_t D>
-T lengthSquared(const Vector<T,D>& vec);
+template<size_t D, typename T>
+T lengthSquared(const Vector<D,T>& vec);
 
 /// \brief Compute the vector's length.
-template <typename T, uint32_t D>
-T length(const Vector<T,D>& vec);
+template<size_t D, typename T>
+T length(const Vector<D,T>& vec);
 
 /// \brief Return the normalized version of a vector without changing it.
-template <typename T, uint32_t D>
-Vector<T,D> normalized(const Vector<T,D>& vec);
+template<size_t D, typename T>
+Vector<D,T> normalized(const Vector<D,T>& vec);
 
 /// \brief Normalize a vector and return the result.
-template <typename T, uint32_t D>
-Vector<T,D>& normalize(Vector<T,D>& vec);
+template<size_t D, typename T>
+Vector<D,T>& normalize(Vector<D,T>& vec);
 
 /***************************************************************************//**
  * \brief Parameterized class that defines a vector (direction in space)
  * 
+ *        Vector extends Array by providing named accessors (x, y, z) and 
+ *        vector arithmetics, namely:
+ *        - Vector addition, subtraction, multiplication, scaling
+ *        - Vector normalization
+ *        - Vector addition with Point class
+ *        - Vector transformation (with Transform class)
+ * 
  * \tparam T The precision of the scalar used to represent the vector
  * \tparam D The number of dimensions of the space the vector is in
  ******************************************************************************/
-template <typename T, uint32_t D>
+template<size_t D, typename T>
 class Vector {
 private:
 	blitz::Array<T, 1> m_data;
