45846553ac3741f1fb5268006fef3c7bae1f0dae
Minh Pham <minh.pham@utexas.edu>
2014-11-19 10:31:34 -0600

    Added test and implementation for equality compare. Floating point implementation doesn't work at the moment.

--- src/core/Math.h
+++ src/core/Math.h
@@ -3,29 +3,93 @@
 
 #include <core/Common.h>
 
+/**
+ * @brief   Compute the sum of arguments
+ * @details Overload for one argument
+ *
+ * @param arg1 The argument
+ * @return  The argument given
+ */
 template<typename T>
 constexpr T sum(T arg1) {
 	return arg1;
 }
 
+/**
+ * @brief [brief description]
+ * @details [long description]
+ *
+ * @param arg1 [description]
+ * @param args [description]
+ * @return [description]
+ */
 template<typename T, typename... Args>
 constexpr uint sum(T arg1, Args... args) {
 	return arg1 + sum(args...);
 }
 
+/**
+ * @brief [brief description]
+ * @details [long description]
+ *
+ * @param arg1 [description]
+ * @return [description]
+ */
 template<typename T>
 constexpr T product(T arg1) {
 	return arg1;
 }
 
+/**
+ * @brief [brief description]
+ * @details [long description]
+ *
+ * @param arg1 [description]
+ * @param args [description]
+ * @return [description]
+ */
 template<typename T, typename... Args>
 constexpr T product(T arg1, Args... args) {
 	return arg1 * product(args...);
 }
 
+/**
+ * @brief [brief description]
+ * @details [long description]
+ *
+ * @param arg1 [description]
+ * @param args [description]
+ * @return [description]
+ */
 template<typename T, typename... Args>
 constexpr uint count(T arg1, Args... args) {
 	return 1 + sizeof...(Args);
+}
+
+/**
+ * @brief   Return true if the arguments are equal
+ * @details [long description]
+ *
+ * @param arg1 [description]
+ * @param arg2 [description]
+ * @return [description]
+ */
+template<typename T1, typename T2>
+constexpr bool eq(T1 arg1, T2 arg2) {
+	return (arg1 == arg2);
+}
+
+/**
+ * @brief   Return true if the arguments are NOT equal
+ * @details [long description]
+ *
+ * @param arg1 [description]
+ * @param arg2 [description]
+ * @return [description]
+ */
+template<typename T1, typename T2>
+constexpr bool neq(T1 arg1, T2 arg2) {
+	return !eq(arg1, arg2);
 }
 
 #endif  // CORE_ARITHMETICS_H
--- test/TestMath.h
+++ test/TestMath.h
@@ -25,5 +25,40 @@ TEST(count, CorrectResult) {
 	EXPECT_EQ(4, count(1, 2, 3, 4));
 }
 
+TEST(eq, CorrectResult) {
+	// Integer comparison
+	EXPECT_TRUE(eq(1, 1));
+	EXPECT_TRUE(eq(0, 0));
+	EXPECT_TRUE(eq(-0, -0));
+	EXPECT_TRUE(eq(-1, -1));
+	EXPECT_TRUE(eq(2 + 3, 1 + 4));
+	EXPECT_TRUE(eq(6 - 10, -4));
+	EXPECT_TRUE(eq(9 / 3, 3));
+	EXPECT_TRUE(eq(9 / 2, 4));
+
+	EXPECT_FALSE(eq(1, 2));
+	EXPECT_FALSE(eq(10 - 9, 0));
+
+	// Floating point comparison
+	EXPECT_TRUE(eq(1.0f, 1.0f));
+	EXPECT_TRUE(eq(0.0f, 0.0f));
+	EXPECT_TRUE(eq(-0.0f, 0.0f));
+	EXPECT_TRUE(eq(-1.0f, -1.0f));
+	EXPECT_TRUE(eq(1.0f / 3.0f * 3.0f, 1.0f));
+	EXPECT_TRUE(eq(12.0f * 5.0f, 3.0f * 20.0f));
+
+	EXPECT_FALSE(eq(1.0f, 1.1f));
+
+	// Floating point inexactness from calculations (YMMV)
+	float f = 0.1f;
+	float sum = 0;
+
+	for (int i = 0; i < 10; ++i)
+    sum += f;
+	float product = f * 10;
+
+	EXPECT_TRUE(eq(sum, product));
+	EXPECT_TRUE(eq(product, f * 10));
+}
 
 #endif  // TEST_TESTMATH_H
