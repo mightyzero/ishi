9b71448b2885c122fe6a649018777b6953de5829
Minh Pham <minh.pham@utexas.edu>
2015-10-30 17:37:16 -0500

    Removed SFML from dependencies.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,3 @@
-#include <SFML/Window.hpp>
-
 int main() {
-	sf::Window window(sf::VideoMode(800, 600), "nFlux");
-
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
-	}
-
 	return 0;
 }
